8e96e1c32ad8f4847aed3e3cc1d5256f0c715adf
Jose <jalopez1423@gmail.com>
2018-07-04 22:29:13 -0700

    refactored

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,9 +13,9 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-//        'teams/create',
-//        'players/create',
-//        'players/update/*',
+        'teams/create',
+        'players/create',
+        'players/update/*',
         'login',
         'logout',
     ];
